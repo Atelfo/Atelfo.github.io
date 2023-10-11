96a1e3c37c10f57f30b7521e1c7e8b2b496710f7
Atelfo <32594694+Atelfo@users.noreply.github.com>
2023-10-11 23:53:34 +0200

    Update landscape.js

--- assets/landscape.js
+++ assets/landscape.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('landscape');
+const canvas = document.getElementById('landscapecanvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
